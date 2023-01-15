1aeb0e70cba0334062584ac59b61bd3248b33bd9
krishna9788 <krishnakushwaha978@gmail.com>
2023-01-15 12:29:05 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Ecommerce Website</h1>
+      <p data-ns-test="project-description">I am build a online fruits and vegetables store</p>
+    </div>
   )
 }
 
